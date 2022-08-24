81d3a2053d019f0268770d58c850eb738eb2d620
zhangyaosen <111969769+zhangyaosen@users.noreply.github.com>
2022-08-24 18:17:44 +0800

    优化代码
    一点点代码

--- FirstGitHubClass.java
+++ FirstGitHubClass.java
@@ -2,6 +2,6 @@ package Java高级APl预习.GitHub;
 
 public class FirstGitHubClass {
     public static void main(String[] args) {
-        System.out.println("Hello,GitHub!");
+        System.out.println("Hello,GitHub! Anger!");
     }
 }
